040d36f5ca800f8004d5efafd348f0a6c52c46af
fengyu415 <969170653@qq.com>
2020-07-18 17:33:25 +0800

    创建mybatis学习源代码

---
+++ MyBatis-Demo-01/src/main/java/com/feng/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.feng.mapper;
+
+import com.feng.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.type.Alias;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+
+    List<User> getUserList();
+    //获取
+    User getUserListById(int id);
+
+    List<User>  getUserListByMap(Map<String, Object> map);
+
+    List<User> getUserListByNP(@Param("name") String name, @Param("pwd") String pwd);
+
+    int addUser(User user);
+
+    int updateUser(User user);
+
+    int deleteUser(int id);
+}
